e69626b17e5a8f6ad265750d418ee868c98255d7
Ignacio Brusati <71679642+brusati@users.noreply.github.com>
2022-06-06 16:53:57 -0300

    update app.py

--- app.py
+++ app.py
@@ -21,3 +21,7 @@ def getSongsFrom(anArtist):
 @app.get('/song/{aSongTitle}')
 def getDescriptionOf(aSongTitle):
 	return list(songs[songs['title'] == aSongTitle]['description'])
+
+# localhost
+# if __name__ == '__main__':
+# 	uvicorn.run(app, port=8000)
