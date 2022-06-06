ea9fdac4263de5b5c6f39dc2ca5efee221c0037f
Ignacio Brusati <71679642+brusati@users.noreply.github.com>
2022-06-06 16:39:03 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+import pandas as pd
+
+songs = pd.read_csv('songs.csv')
+
+app = FastAPI()
+
+@app.get('/songs')
+def index():
+	return list(songs['title'])
+
+@app.get('/ping')
+def ping():
+	return 'pong'
+
+@app.get('/artist/{anArtist}')
+def getSongsFrom(anArtist):
+	return list(songs[songs['artist'] == anArtist]['title'])
+
+@app.get('/song/{aSongTitle}')
+def getDescriptionOf(aSongTitle):
+	return list(songs[songs['title'] == aSongTitle]['description'])
+
+if __name__ == '__main__':
+	uvicorn.run(app, port=8000)
